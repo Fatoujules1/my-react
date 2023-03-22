58870af2b2f8fa7f2ee4e5221edf808e7af5cb92
souley <boysouley1@hotmail.com>
2023-03-21 21:05:31 -0400

    updated app

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Learn <code>src/App.js</code> and save to reload.
-        </p>
-        <input />
-      </header>
+      <h1>this is my first react app</h1>
     </div>
   );
 }
